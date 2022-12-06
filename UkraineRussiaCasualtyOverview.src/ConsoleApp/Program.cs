d6cb7a93fe0faf370dcdb2f198ae05be86a36f52
= <thorchr96@gmail.com>
2022-12-06 21:27:32 +0100

    Added ability to see deadliest month.

--- UkraineRussiaCasualtyOverview.src/ConsoleApp/Program.cs
+++ UkraineRussiaCasualtyOverview.src/ConsoleApp/Program.cs
@@ -53,19 +53,59 @@ Console.WriteLine($"\t{daysNeeded}");
 #endregion
 
 #region Casualties in 1 year of war
-
+// Get the dates we need.
 DateOnly warStartDate = DateOnly.Parse("24.02.2022");
 DateOnly currentDate = DateOnly.FromDateTime(DateTime.Now);
+
+// Find out how many more days the war needs to continue till we reach 1 full year.
 int daysWarActive = currentDate.DayNumber - warStartDate.DayNumber;
 int daysInYear = DateTime.IsLeapYear(DateTime.Now.Year) ? 366 : 365;
 int daysLeftTill1Year = daysInYear - daysWarActive;
+
+// Handle getting the average for total deaths.
 int totalAverageDeaths = totalDeaths / daysWarActive;
 
+// Extrapolating.
 int totalDeathsAllAverage = totalDeaths + (daysLeftTill1Year * totalAverageDeaths);
 int totalDeathsSevenDayAverage = totalDeaths + (daysLeftTill1Year * lastSevenDaysAverage);
 
 Console.WriteLine("\nTotal projected casualties for 1 year of war: ");
 Console.WriteLine($"\tUsing total average deaths: {totalDeathsAllAverage}");
 Console.WriteLine($"\tUsing last 7 day average deaths: {totalDeathsSevenDayAverage}");
+
+#endregion
+
+#region Deadliest month for Russia
+
+// Create array of months, and fill it with deaths.
+int[] monthlyCasualties = new int[12];
+foreach(var item in casualtyWrapper.Data)
+{
+    int currentMonth = item.Key.Month - 1;
+    monthlyCasualties[currentMonth] += item.Value.Personnel; 
+}
+
+// Now get some data for the best month.
+int deadliestMonthIndex = monthlyCasualties.ToList().IndexOf(monthlyCasualties.Max());
+int deadliestMonthBodycount = monthlyCasualties.Max();
+string monthName = deadliestMonthIndex switch
+{
+    0 => "January",
+    1 => "Febuary",
+    2 => "March",
+    3 => "April",
+    4 => "May",
+    5 => "June",
+    6 => "July",
+    7 => "August",
+    8 => "September",
+    9 => "October",
+    10 => "November",
+    11 => "December",
+    _ => "Unknown month"
+};
+
+Console.WriteLine("\nThe deadliest month for Russia was: ");
+Console.WriteLine($"{monthName} with a total of {deadliestMonthBodycount} deaths.");
 
 #endregion
